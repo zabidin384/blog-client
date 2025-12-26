fa469d2887b095e654d0a261579330a7691acc0a
Zainal Abidin <zabidin384@gmail.com>
2025-12-26 23:25:22 +0700

    Change UI register page

--- src/routes/RegisterPage.jsx
+++ src/routes/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 const RegisterPage = () => {
 	return (
-		<div className="flex items-center justify-center h-[calc(100vh-80px)]">
+		<div className="flex items-center justify-center min-h-[calc(100vh-80px)] pb-8">
 			<SignUp signInUrl="/login" />
 		</div>
 	);
