e2185873035697b1d25ba513fe652d4ff5c022a7
Zainal Abidin <zabidin384@gmail.com>
2025-12-28 12:05:31 +0700

    Fix handle category for all posts

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,7 +5,7 @@ const Categories = ({ handleCategoryChange }) => {
 		<>
 			<h1 className="mt-4 mb-2 text-sm font-semibold">Categories</h1>
 			<div className="flex flex-col gap-2 text-sm">
-				<span className="hover:underline cursor-pointer" onClick={() => handleCategoryChange("general")}>
+				<span className="hover:underline cursor-pointer" onClick={() => handleCategoryChange("")}>
 					- All Posts
 				</span>
 				{categories.map((category, i) => (
