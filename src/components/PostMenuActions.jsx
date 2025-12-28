a9e1394b0a4db2e58b2d7762258af2717a9118d9
Zainal Abidin <zabidin384@gmail.com>
2025-12-28 12:25:43 +0700

    Fix get save post, eror, and toast success in post menu actions

--- src/components/PostMenuActions.jsx
+++ src/components/PostMenuActions.jsx
@@ -5,19 +5,15 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { PulseLoader } from "react-spinners";
 import { IoIosStar, IoIosStarOutline } from "react-icons/io";
-import { MdDelete } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
 
 const PostMenuActions = ({ post }) => {
 	const { user } = useUser();
 	const { getToken } = useAuth();
 	const navigate = useNavigate();
 
-	// Get
-	const {
-		isPending,
-		error,
-		data: savedPosts,
-	} = useQuery({
+	// Get Saved Posts
+	const { error, data: savedPosts } = useQuery({
 		queryKey: ["savedPosts"],
 		queryFn: async () => {
 			const token = await getToken();
@@ -26,7 +22,7 @@ const PostMenuActions = ({ post }) => {
 	});
 
 	const isAdmin = user?.publicMetadata?.role === "admin" || false;
-	const isSaved = savedPosts?.data?.some((p) => p === post._id) || false;
+	const isSaved = savedPosts?.data?.posts?.some((p) => p._id === post?._id) || false;
 	const isFeatured = post?.isFeatured;
 
 	// Delete Post
@@ -54,7 +50,10 @@ const PostMenuActions = ({ post }) => {
 				{ headers: { Authorization: `Bearer ${token}` } }
 			);
 		},
-		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["savedPosts"] }),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["savedPosts"] });
+			toast.success("Post has been saved successfully!");
+		},
 		onError: (error) => toast.error(error.response.data),
 	});
 
@@ -73,17 +72,29 @@ const PostMenuActions = ({ post }) => {
 				{ headers: { Authorization: `Bearer ${token}` } }
 			);
 		},
-		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["post", post.slug] }),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["post", post.slug] });
+			toast.success("Post has been featured successfully!");
+		},
 		onError: (error) => toast.error(error.response.data),
 	});
 
 	return (
 		<div className="w-max pr-4">
-			<h1 className="mt-8 mb-4 text-sm font-medium">Actions</h1>
-			{isPending ? (
-				"Loading..."
-			) : error ? (
-				"Saved post fetching failed!"
+			<h1 className="mt-8 mb-4 text-sm font-semibold">Actions</h1>
+			{/* Save Post */}
+			{error ? (
+				<div className="flex items-center gap-2 py-2 text-sm group cursor-not-allowed">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20px" height="20px">
+						<path
+							d="M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z"
+							stroke="black"
+							strokeWidth="3"
+							fill={saveMutation.isPending ? (isSaved ? "none" : "black") : isSaved ? "black" : "none"}
+						/>
+					</svg>
+					<span className="text-sm">Save post error!</span>
+				</div>
 			) : (
 				<div className="flex items-center gap-2 py-2 text-sm cursor-pointer group" onClick={handleSave}>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20px" height="20px">
@@ -98,6 +109,8 @@ const PostMenuActions = ({ post }) => {
 					<PulseLoader loading={saveMutation.isPending} size={5} speedMultiplier={0.75} />
 				</div>
 			)}
+
+			{/* Featured Post */}
 			{isAdmin && (
 				<div className="flex items-center gap-2 py-2 text-sm cursor-pointer" onClick={() => featureMutation.mutate()}>
 					{isFeatured ? (
@@ -114,9 +127,11 @@ const PostMenuActions = ({ post }) => {
 					<PulseLoader loading={featureMutation.isPending} size={5} speedMultiplier={0.75} />
 				</div>
 			)}
+
+			{/* Delete Post */}
 			{user && (post?.user?.username === user.username || isAdmin) && (
 				<div className="group flex items-center gap-2 py-2 text-sm cursor-pointer" onClick={() => deleteMutation.mutate()}>
-					<MdDelete className="text-red-700 group-hover:text-red-600" size={20} />
+					<MdDeleteOutline size={20} />
 					<span className="group-hover:underline">Delete</span>
 					<PulseLoader loading={deleteMutation.isPending} size={5} speedMultiplier={0.75} />
 				</div>
