50cc2018eb2ae68109c38e19803918ed1b9ae31e
Zainal Abidin <zabidin384@gmail.com>
2025-12-28 12:03:07 +0700

    Add build settings in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					clerk: ["@clerk/clerk-react"],
+					vendor: ["react", "react-dom", "react-router-dom"],
+				},
+			},
+		},
+	},
+});
